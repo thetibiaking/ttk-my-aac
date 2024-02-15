eae160d3cf2aa615881e963f1b057eb040ec9e82
Rafhael Oliveira <rafhaelxd@gmail.com>
2024-02-15 11:22:07 -0300

    Dev: Automatic mercadopagoPIX System Gatway

---
+++ plugins/mercadopago/pix/config.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Automatic Mercadopago payment system gateway.
+ *
+ * @name      pix-myaac-mercadopago
+ * @author    Rafhael Oliveira <rafhaelxd@gmail.com>
+ * @website   github.com/thetibiaking/ttk-myaac
+ * @website   github.com/underewarrr/
+ * @version   1.0.0
+ */
+
+ $pointsByDonate = 60; //  coins
+ $priceByPoints = 25.00; //  value
+ $conversionRate = $pointsByDonate / $priceByPoints; 
+ $debug = false;
+ $notificationUrl = 'https://mywebsite.online/?mpnotification';
+ $noSessionUrl = 'https://mywebsite.online/?account/manage';
+ $donationType = 'premium_points'; // coins or premium_points
+ $doublePoints = false; // not added yet
+ $authorizationToken = 'APP_USR-YOURTOKEN';
+
+$servername     = "localhost"; 
+$username       = "root";
+$password       = "";
+$db_name        = "";
+
+$conexao = mysqli_connect($servername, $username, $password, $db_name);
+
+if (!$conexao) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+
+?>
+
+
+
+
+
+
+
+
+
+
